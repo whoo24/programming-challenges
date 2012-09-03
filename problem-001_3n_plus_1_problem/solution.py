2941742c888b8187d2a5c930be5bc3854271c9b6
whoo24 <whoo24@gmail.com>
2012-09-03 15:30:14 +0900

    runable, but time limit

--- problem-001_3n_plus_1_problem/solution.py
+++ problem-001_3n_plus_1_problem/solution.py
@@ -1,3 +1,5 @@
+from sys import stdin, stdout
+
 def is_odd(n):
     if (n % 2) == 0:
         return False
@@ -8,21 +10,39 @@ def func(n, length):
     if n == 1:
         return length
     
+    #if is_odd(n):
+    #    n = n * 3 + 1
+    #else:
+    #    n = n >> 1
+
     if is_odd(n):
         n = n * 3 + 1
-    else:
-        n = n / 2
+        length += 1
+        
+    n = n >> 1
     
     return func(n, length+1)
 
 def solve(i, j):
+
     length = 0
     for k in range(i, j):
         length = max( length, func(k, 1) )
     return length
 
 # 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
-i = 100
-j = 200
-print '%d %d %d' % (i, j, solve(i, j) )
+inputs = []
+while(True):
+    try:
+        line = raw_input()
+        
+        a, b = line.split()
+        inputs.append( [int(a), int(b)] )
 
+    except Exception, e:
+        break;
+
+for input_pair in inputs:
+    i, j = min( input_pair[0], input_pair[1] ), max( input_pair[0], input_pair[1] )
+
+    print( '%d %d %d' % ( input_pair[0], input_pair[1], solve(i, j) ) )
